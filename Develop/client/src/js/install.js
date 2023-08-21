028ebb1cafe33d25308fb91f46f014d2b985e547
Luceatscode <lucsw92@hotmail.com>
2023-08-21 16:56:03 -0600

    added logic/click events to install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,34 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // save events for later
+    window.deferredPrompt = event;
+
+    // remove the hidden attribute from the install button
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        // The deferred prompt isn't available.
+        return;
+    }
+
+    // Show the install prompt.
+    promptEvent.prompt();
+
+    // reset the deferred prompt variable since it can only be used once.
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Log install to analytics
+    console.log('Jate was installed', event);
+});
